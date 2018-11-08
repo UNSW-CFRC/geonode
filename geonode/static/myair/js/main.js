c9a4dc5b903cf587f8ae3d80f0d10c2a6235f5d0
Jonathan Doig <z8290362@ad.unsw.edu.au>
2018-11-08 11:04:14 +1100

    Remove pre-JQuery code comments

--- geonode/static/myair/js/main.js
+++ geonode/static/myair/js/main.js
@@ -1,17 +1,9 @@
-// var slider = document.getElementById('myRange');
-// var squiggleElem = document.getElementById('squiggle');
-// var valElem = document.getElementById('val');
-// var valTextElem = document.getElementById('valText');
-// valElem.innerHTML = slider.value;
 $('#val').html($('#myRange').value);
 
 $('#myRange').on('input', function() {
-// slider.oninput = function() {
   $('#val').html(this.value);
-  // valElem.innerHTML = this.value;
 
   if (this.value < 600) {
-      // valTextElem.innerHTML = 'very good';
       $('#valText').html('very good');
     } else if (this.value < 1000) {
       $('#valText').html('good');
